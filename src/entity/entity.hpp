110e3dd7aecda5575a9b6e303a143cebdd76de47
Kumar Saurabh <kumarsaurabh27d@gmail.com>
2026-09-12 15:06:22 +0530

    Complete src/entity/entity.hpp implementation

--- src/entity/entity.hpp
+++ src/entity/entity.hpp
@@ -40,3 +40,46 @@ public:
     ~Entity() {}
 
     void handleEvent(SDL_Event& event, std::shared_ptr<GameClient> client);
+    void update();
+    void render(SDL_Renderer* renderer);
+
+    void reset();
+
+    // delta time
+    float delataTime = 1.0f;
+
+    // Setters
+    void setX(float x) { m_Xpos = x; }
+    void setY(float y) { m_Ypos = y; }
+    void setWidth(int w) { m_Texture->setWidth(w); }
+    void setHeight(int h) { m_Texture->setHeight(h); }
+    void setSpeed(std::pair<float,float> s) { m_Xspeed = s.first; m_Yspeed = s.second; }
+    void setVelocity(std::pair<float,float> v) { m_Xvel = v.first; m_Yvel = v.second; }
+    void setAlive(bool a) { m_Alive = a; }
+
+    // Getters
+    enum Type getType() { return m_Type; }
+    int getX() { return m_Xpos; }
+    int getY() { return m_Ypos; }
+    int getWidth() { return m_Texture->getWidth(); }
+    int getHeight() { return m_Texture->getHeight(); }
+    std::pair<float,float> getSpeed() { return {m_Xspeed, m_Yspeed}; }
+    std::pair<float,float> getVelocity() { return {m_Xvel, m_Yvel}; }
+    bool isAlive() { return m_Alive; }
+
+private:
+
+    std::shared_ptr<GameClient> m_Client = nullptr;
+
+    std::unique_ptr<Texture> m_Texture;
+
+    enum Type m_Type;
+    float m_Xpos, m_Ypos;
+    float m_Xvel, m_Yvel;
+    float m_Xspeed, m_Yspeed;
+    bool m_Alive;
+};
+
+\
+
+#endif // ENTITY_HPP
