1a691565ee260b9a531b5006f88a485117681678
Kumar Saurabh <kumarsaurabh27d@gmail.com>
2026-09-12 15:08:15 +0530

    Add src/net/common.hpp

---
+++ src/net/common.hpp
@@ -0,0 +1,56 @@
+#include "./asio-framework/net.hpp"
+#include <array>
+#include <cstdint>
+#include <vector>
+
+enum class GameMsg : uint32_t
+{
+    Server_Accept,
+    Server_Deny,
+    Server_Ping,
+    Message_All,
+    Server_Message,
+    Client_Accepted,
+
+    Reset_Game,
+
+    Score_Update,
+    Player_Update,
+    Enemy_Update,
+    Ball_Update,
+};
+
+/*
+
+    Message Body For Player_Update
+    Player_X, Player_Y, Player_Score, Enemy_Score
+    Player_Velocity_X, Player_Velocity_Y
+*/
+
+enum class MessageType
+{
+    Text,
+    Binary,
+    Command,
+};
+
+struct sMessageType
+{
+    uint32_t nID;
+    uint32_t nSize;
+    uint32_t nType;
+    std::array<uint8_t, 1024> data;
+
+    void copyToServerBuffer(wkd::net::message<GameMsg>& msg, const std::string& message) {
+        this->nSize = message.size();
+        this->nType = 1;
+        std::memcpy(data.data(), message.data(), message.size());
+        msg << *this;
+    }
+};
+
+
+struct InternalMessageType {
+    GameMsg id;
+    std::string message;
+};
