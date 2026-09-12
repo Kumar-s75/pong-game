843212df1df95c9e92b21630ee0b1a90e145b731
Kumar Saurabh <kumarsaurabh27d@gmail.com>
2026-09-12 15:07:01 +0530

    Complete src/game/game_impl.cc implementation

--- src/game/game_impl.cc
+++ src/game/game_impl.cc
@@ -98,3 +98,103 @@ void Game::update() {
         else if(mess.id == GameMsg::Ball_Update) {
             float xPos, yPos, xVel, yVel;
             ParseBallUpdateMessage(mess.message, xPos, yPos, xVel, yVel);
+            TransformBallCoordinates(xPos, yPos, xVel, yVel);
+            m_EntityManager->setBallData(xPos, yPos, xVel, yVel);
+        }
+
+        // Reset Game
+        else if(mess.id == GameMsg::Reset_Game) {
+            resetGame();
+        }
+    }
+}
+
+// Add Entity to the Entity Manager
+void Game::addEntity(const std::string& path, Entity::Type t)
+{
+    this->m_EntityManager->addEntity(path, m_Renderer, t);
+}
+
+void Game::render(float& deltaTime) {
+
+    // Clear the screen
+    SDL_SetRenderDrawColor(m_Renderer, 255, 255, 255, 255); // Set background color to white
+    SDL_RenderClear(m_Renderer);
+
+    // Render Background
+    m_BackgroundTexture->render(m_Renderer, 0, 0);
+
+    // Draw game objects here
+    m_EntityManager->render(m_Renderer);
+
+    // Draw HUD
+    m_Hud->render(deltaTime);
+
+    // Present the rendered frame
+    SDL_RenderPresent(m_Renderer);
+
+}
+
+void Game::resetGame()
+{
+    isGameRunning = false;
+    playerScore = 0;
+    enemyScore = 0;
+    m_EntityManager->reset();
+}
+
+void Game::clean() {
+    // No need to explicitly destroy gWindow and gRenderer,
+    // as the unique_ptrs will handle it automatically
+}
+
+void Game::ParseScoreUpdate(const std::string& message, int player, int enemy) {
+    // Assuming the message format is "PLAYER: <player>\tENEMY: <enemy>"
+    size_t playerStart = message.find("PLAYER: ") + 8;
+    size_t playerEnd = message.find("\t", playerStart);
+    player = std::stoi(message.substr(playerStart, playerEnd - playerStart));
+
+    size_t enemyStart = message.find("ENEMY: ") + 7;
+    enemy = std::stoi(message.substr(enemyStart));
+}
+
+void Game::ParseEnemyUpdateMessage(const std::string& message, float& xPos, float& yPos) {
+        // Assuming the message format is "Xpos: <xPos>\tYpos: <yPos>"
+        size_t xPosStart = message.find("Xpos: ") + 6;
+        size_t xPosEnd = message.find("\t", xPosStart);
+        xPos = std::stoi(message.substr(xPosStart, xPosEnd - xPosStart));
+
+        size_t yPosStart = message.find("Ypos: ") + 6;
+        yPos = std::stoi(message.substr(yPosStart));
+    }
+
+void Game::TransformEnemyCoordinates(float& xPos, float& yPos) {
+        // Example transformation: mirror the position horizontally and vertically
+        xPos = SCREEN_WIDTH - xPos;
+        yPos = SCREEN_HEIGHT - yPos;
+}
+
+void Game::ParseBallUpdateMessage(const std::string& message, float& xPos, float& yPos, float& xVel, float& yVel) {
+    size_t xPosStart = message.find("Xpos: ") + 6;
+    size_t xPosEnd = message.find("\t", xPosStart);
+    xPos = std::stof(message.substr(xPosStart, xPosEnd - xPosStart));
+
+    size_t yPosStart = message.find("Ypos: ") + 6;
+    size_t yPosEnd = message.find("\t", yPosStart);
+    yPos = std::stof(message.substr(yPosStart, yPosEnd - yPosStart));
+
+    size_t xVelStart = message.find("Xvel: ") + 6;
+    size_t xVelEnd = message.find("\t", xVelStart);
+    xVel = std::stof(message.substr(xVelStart, xVelEnd - xVelStart));
+
+    size_t yVelStart = message.find("Yvel: ") + 6;
+    yVel = std::stof(message.substr(yVelStart));
+}
+
+void Game::TransformBallCoordinates(float& xPos, float& yPos, float& xVel, float& yVel) {
+    // Example transformation: mirror the position horizontally and vertically
+    xPos = SCREEN_WIDTH - xPos;
+    yPos = SCREEN_HEIGHT - yPos;
+    xVel = -xVel;
+    yVel = -yVel;
+}
