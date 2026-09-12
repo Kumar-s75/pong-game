c57d974b7d52d0129b3d132c18a34b6deaae9c49
Kumar Saurabh <kumarsaurabh27d@gmail.com>
2026-09-12 15:07:45 +0530

    Complete src/net/asio-framework/net_connection.hpp implementation

--- src/net/asio-framework/net_connection.hpp
+++ src/net/asio-framework/net_connection.hpp
@@ -170,3 +170,176 @@ namespace wkd
                     }
                 });
             }
+
+            // ASYNC - Prime context to write a message header
+            void WriteHeader()
+            {
+                asio::async_write(m_socket, asio::buffer(&m_qMessageOut.front().header, sizeof(message_header<T>)),
+                [this](std::error_code ec, std::size_t length)
+                {
+                    if(!ec)
+                    {
+                        if(m_qMessageOut.front().body.size() > 0)
+                        {
+                            WriteBody();
+                        }
+                        else
+                        {
+                            m_qMessageOut.pop_front();
+                            if(!m_qMessageOut.empty())
+                            {
+                                WriteHeader();
+                            }
+                        }
+                    }
+                    else
+                    {
+                        std::cout << "[" << m_id << "] Write Header Fail." << std::endl;
+                        m_socket.close();
+                    }
+                });
+            }
+
+            // ASYNC - Prime context to write a message body
+            void WriteBody()
+            {
+                asio::async_write(m_socket, asio::buffer(m_qMessageOut.front().body.data(), m_qMessageOut.front().body.size()),
+                [this](std::error_code ec, std::size_t length)
+                {
+                    if(!ec)
+                    {
+                        m_qMessageOut.pop_front();
+                        if(!m_qMessageOut.empty())
+                        {
+                            WriteHeader();
+                        }
+                    }
+                    else
+                    {
+                        std::cout << "[" << m_id << "] Write Body Fail." << std::endl;
+                        m_socket.close();
+                    }
+                });
+            }
+
+            // Add incoming message to the queue
+            void AddToIncomingMessageQueue()
+            {
+                if(m_nOwnerType == owner::server)
+                {
+                    m_qMessageIn.push_back({this->shared_from_this(), m_msgTemporaryIn});
+                }
+                else
+                {
+                    m_qMessageIn.push_back({nullptr, m_msgTemporaryIn});
+                }
+
+                // Register Another task for asio to read another header
+                ReadHeader();
+            }
+
+            // "Encrypt" fata
+            uint64_t Scramble(uint64_t nInput)
+            {
+                uint64_t out = nInput ^ 0xDEADBEEFC0DECAFE;
+				out = (out & 0xF0F0F0F0F0F0F0) >> 4 | (out & 0x0F0F0F0F0F0F0F) << 4;
+				return out ^ 0xC0DEFACE12345678;
+            }
+
+            // ASYNC - Write Validation Packet, used by both client and server
+            void WriteValidation()
+            {
+                asio::async_write(m_socket, asio::buffer(&m_nHandshakeOut, sizeof(uint64_t)),
+                    [this](std::error_code ec, std::size_t length)
+                    {
+                        if(!ec)
+                        {
+                            // Validation data sent, wait for a response
+                            if(m_nOwnerType == owner::client)
+                            {
+                                ReadHeader();
+                            }
+                        }
+                        else
+                        {
+                            m_socket.close();
+                        }
+                    });
+            }
+
+            // ASYNC - Read Validation Packet, used by server
+            void ReadValidation(wkd::net::server_interface<T>* server = nullptr)
+            {
+                asio::async_read(m_socket, asio::buffer(&m_nHandshakeIn, sizeof(uint64_t)),
+                    [this, server](std::error_code ec, std::size_t length)
+                    {
+                        if(!ec)
+                        {
+                            if(m_nOwnerType == owner::server)
+                            {
+                                // Connection is from server
+                                if(m_nHandshakeIn == m_nHandshakeCheck)
+                                {
+                                    // Client has solved the validation challenge
+                                    std::cout << "[SERVER] Client Validated" << std::endl;
+                                    server->OnClientValidated(this->shared_from_this());
+
+                                    // Connection is now validated, so start reading messages
+                                    ReadHeader();
+                                }
+                                else
+                                {
+                                    // Client validation failed
+                                    std::cout << "[SERVER] Client Disconnected (Fail Validation)" << std::endl;
+                                    m_socket.close();
+                                }
+                            }
+                            else
+                            {
+                                // Connection is Client, so solve the validation
+                                m_nHandshakeOut = Scramble(m_nHandshakeIn);
+                                // Write the result
+                                WriteValidation();
+                            }
+                        }
+                        else
+                        {
+                            // Terminate the connection
+                            std::cout << "[CLIENT DISCONNECTED] Read Validation Failed: " << ec.message() << std::endl;
+                            m_socket.close();
+                        }
+                    });
+            }
+
+        protected:
+            // each connection has a unique socket to remote
+            asio::ip::tcp::socket m_socket;
+
+            // this context is shared with the whole asio instance
+            asio::io_context& m_asioContext;
+
+            // holds all the messages to be sent to remote
+            thread_safe_queue<message<T>> m_qMessageOut;
+
+            // holds all the messages that have been recieved from the remote side of this connection
+            // NOTE: It is a reference as the "owner" of this connection is expected to provide a queue
+            thread_safe_queue<owned_message<T>>& m_qMessageIn;
+
+            // temporary incoming message buffer
+            message<T> m_msgTemporaryIn;
+
+            // owner of the connection
+            owner m_nOwnerType = owner::server;
+            // Identifier
+            uint32_t m_id = 0;
+
+            // Handshake Validation
+            uint64_t m_nHandshakeOut = 0;
+            uint64_t m_nHandshakeIn = 0;
+            uint64_t m_nHandshakeCheck = 0;
+        };
+    }
+}
+
+
+#endif // NET_CONNECTION_HPP
