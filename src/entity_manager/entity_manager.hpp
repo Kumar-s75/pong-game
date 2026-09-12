de6dfb7ac4d6bd9d962eb27901d29bedeb810dbb
Kumar Saurabh <kumarsaurabh27d@gmail.com>
2026-09-12 15:06:33 +0530

    Add initial src/entity_manager/entity_manager.hpp implementation

---
+++ src/entity_manager/entity_manager.hpp
@@ -0,0 +1,20 @@
+#ifndef ENTITY_MANAGER_HPP
+#define ENTITY_MANAGER_HPP
+
+#include "../entity/entity.hpp"
+#include "../sound/sound.hpp"
+#include "../net/client.hpp"
+
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_render.h>
+#include <memory>
+#include <vector>
+
+class EntityManager {
+public:
+    EntityManager(int s_width, int s_height, int& playerScore, int& enemyScore)
+    : m_SCREEN_WIDTH(s_width), m_SCREEN_HEIGHT(s_height), m_PlayerScore(playerScore), m_EnemyScore(enemyScore)
+    {}
+    void addEntity(const std::string& path, SDL_Renderer* renderer, Entity::Type t);
+    void handleEvent(SDL_Event& event, std::shared_ptr<GameClient> client);
+    void handleCollison(Sound* gameSound, bool&);
