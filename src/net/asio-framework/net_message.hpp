96dde2a5298bcf50e797b5e5d20b459ad0a73511
Kumar Saurabh <kumarsaurabh27d@gmail.com>
2026-09-12 15:07:52 +0530

    Complete src/net/asio-framework/net_message.hpp implementation

--- src/net/asio-framework/net_message.hpp
+++ src/net/asio-framework/net_message.hpp
@@ -56,3 +56,62 @@ namespace wkd
 
                 // Resize the vector by the size of the data being pushed
                 msg.body.resize(msg.body.size() + sizeof(DataType));
+
+                // Physically copy the data into the newly allocated vector space
+                std::memcpy(msg.body.data()+i, &data, sizeof(DataType));
+
+                // Recalculate the message size
+                msg.header.size = msg.size();
+
+                // Return the target message so it can be "chained"
+                return msg;
+            }
+
+            // Extract data from the vector
+            template <typename DataType>
+            friend message<T>& operator >> (message<T>& msg, DataType& data)
+            {
+                // Check that the type of the data being pushed is trivially copyable
+				static_assert(std::is_standard_layout<DataType>::value, "Data is too complex to be pulled from vector");
+
+				// Cache the location towards the end of the vector where the pulled data starts
+				size_t i = msg.body.size() - sizeof(DataType);
+
+				// Physically copy the data from the vector into the user variable
+				std::memcpy(&data, msg.body.data() + i, sizeof(DataType));
+
+				// Shrink the vector to remove read bytes, and reset end position
+				msg.body.resize(i);
+
+				// Recalculate the message size
+				msg.header.size = msg.size();
+
+				// Return the target message so it can be "chained"
+				return msg;
+            }
+        };
+
+        // Forward declare the connection type
+        template <typename T>
+        class connection;
+
+        // An "owned" message is identical to a regular message, but it is associated with
+		// a connection. On a server, the owner would be the client that sent the message,
+		// on a client the owner would be the server.
+
+        template <typename T>
+        struct owned_message
+        {
+            std::shared_ptr<connection<T>> remote = nullptr;
+            message<T> msg;
+
+            friend std::ostream& operator << (std::ostream& os, const owned_message<T>& msg)
+            {
+                os << msg.msg;
+                return os;
+            }
+        };
+    }
+}
+
+#endif // NET_MESSAGE_HPP
