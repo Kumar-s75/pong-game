dca3d38311b82124b6d985f8f9f3f08af7d87e0f
Kumar Saurabh <kumarsaurabh27d@gmail.com>
2026-09-12 15:08:00 +0530

    Complete src/net/asio-framework/net_server.hpp implementation

--- src/net/asio-framework/net_server.hpp
+++ src/net/asio-framework/net_server.hpp
@@ -105,3 +105,111 @@ namespace wkd
             void MessageClient(std::shared_ptr<connection<T>> client, const message<T>& msg)
             {
                 if(client && client->IsConnected())
+                {
+                    client->Send(msg);
+                }
+                else
+                {
+                    // Remove client
+                    OnClientDisconnect(client);
+                    client.reset();
+                    m_deqConnections.erase(
+                        std::remove(m_deqConnections.begin(), m_deqConnections.end(), client), m_deqConnections.end());
+                }
+            }
+
+            // Send to all clients
+            void MessageAllClients(const message<T>& msg, std::shared_ptr<connection<T>> pIgnoreClient = nullptr)
+            {
+                bool bInvalidClientExists = false;
+
+                for (auto& client: m_deqConnections)
+                {
+                    // Check client is connected...
+                    if (client && client->IsConnected())
+                    {
+                        if(client != pIgnoreClient)
+                            client->Send(msg);
+                    }
+                    else
+                    {
+                        // The client couldn't be contacted, so assume it is disconnected
+                        OnClientDisconnect(client);
+                        client.reset();
+                        bInvalidClientExists = true;
+                    }
+                }
+
+                if(bInvalidClientExists)
+                {
+                    // remove client from the deque
+                    m_deqConnections.erase(
+                        std::remove(m_deqConnections.begin(), m_deqConnections.end(), nullptr), m_deqConnections.end());
+                }
+            }
+
+            // Force server to respond to incoming messages
+            void Update(size_t nMaxMessages = -1, bool bWait = false)
+            {
+                // Optimize CPU Utilization
+                if(bWait) m_qMessageIn.wait();
+
+                size_t nMessageCount = 0;
+                while (nMessageCount < nMaxMessages && !m_qMessageIn.empty())
+                {
+                    // extract the owned message
+                    auto msg = m_qMessageIn.pop_front();
+
+                    // Pass it on to message handler
+                    OnMessage(msg.remote, msg.msg);
+                    nMessageCount++;
+                }
+            }
+
+        protected:
+            // Called when a client connects, you can veto the connection by returning false
+            virtual bool OnClientConnect(std::shared_ptr<connection<T>> client)
+            {
+                return false;
+            }
+
+            // Called when a client appears to have disconnected
+            virtual void OnClientDisconnect(std::shared_ptr<connection<T>> client)
+            {
+
+            }
+
+            // Called when a message arrives
+            virtual void OnMessage(std::shared_ptr<connection<T>> client, message<T>& msg)
+            {
+
+            }
+
+        public:
+            // Called when the client is validated
+            virtual void OnClientValidated(std::shared_ptr<connection<T>> client)
+            {
+
+            }
+
+        protected:
+            // Thread safe queue for incoming message packets
+            thread_safe_queue<owned_message<T>> m_qMessageIn;
+
+            // Container of active validated connections
+            std::deque<std::shared_ptr<connection<T>>> m_deqConnections;
+
+            // Order of declaration is important - it is also the order of initialization
+            asio::io_context m_asioContext;
+            std::thread m_thrContext;
+
+            // Needs an asio context
+            asio::ip::tcp::acceptor m_asioAcceptor;
+
+            // Clients will be identified "system wide" via an ID
+            uint32_t nIDCounter = 10000;
+        };
+    }
+}
+
+#endif // NET_SERVER_HPP
