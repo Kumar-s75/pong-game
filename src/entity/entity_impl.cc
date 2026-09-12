955eb9afec94d15a9bf0e9e4e163459c57682635
Kumar Saurabh <kumarsaurabh27d@gmail.com>
2026-09-12 15:06:30 +0530

    Complete src/entity/entity_impl.cc implementation

--- src/entity/entity_impl.cc
+++ src/entity/entity_impl.cc
@@ -52,3 +52,58 @@ void Entity::handleEvent(SDL_Event& event, std::shared_ptr<GameClient> client)
                     m_Xvel = m_Xspeed * delataTime;
                     break;
                 default:
+                    break;
+            }
+        }
+        else if(event.type == SDL_KEYUP) {
+            switch(event.key.keysym.sym) {
+                case SDLK_a:
+                    m_Xvel = 0;
+                    break;
+                case SDLK_d:
+                    m_Xvel = 0;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+}
+
+void Entity::reset()
+{
+    if(m_Type == Entity::Type::PLAYER) {
+        m_Xpos = m_SCREEN_WIDTH / 2 - 25;
+        m_Ypos = m_SCREEN_HEIGHT - 70;
+    }
+    else if(m_Type == Entity::Type::ENEMY) {
+        m_Xpos = m_SCREEN_WIDTH / 2 - 25;
+        m_Ypos = 60;
+    }
+    else if(m_Type == Entity::Type::BALL) {
+        m_Xpos = m_SCREEN_WIDTH/2;
+        m_Ypos = m_SCREEN_HEIGHT/2;
+    }
+}
+
+void Entity::update()
+{
+    // prev vals;
+    float prev_x = m_Xpos, prev_y = m_Ypos;
+
+    m_Xpos += m_Xvel;
+    m_Ypos += m_Yvel;
+
+    if(m_Client && m_Client->IsConnected() && this->m_Type == Type::PLAYER) {
+        if(prev_x == m_Xpos && prev_y == m_Ypos) {}
+        else {
+            m_Client->SendPlayerData(m_Xpos, m_Ypos);
+        }
+    }
+
+    else if(m_Client && m_Client->IsConnected() && this->m_Type == Type::BALL) {
+        m_Client->SendBallData(m_Xpos, m_Ypos, m_Xvel, m_Yvel);
+    }
+
+}
